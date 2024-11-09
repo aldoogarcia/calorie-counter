1046817e63659fa74e5aceb2eac93cdab615c74c
aldo garcia <aldoredcreo@gmail.com>
2024-11-08 19:28:00 -0600

    Start project

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+
+
+function App() {
+
+
+  return (
+    <>
+      <header className="w-full py-3 bg-green-500 font-medium my-auto">
+        <div className="max-w-5xl mx-auto flex justify-between ">
+            <p className=" p-1">
+              CaloriesCounter
+            </p>
+            <button className="border border-1 border-green-800 p-1 ">Reiniciar</button>
+        </div>
+      </header>
+
+      <section className="w-full bg-green-700">
+        <div className="max-w-4xl">
+            
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default App
